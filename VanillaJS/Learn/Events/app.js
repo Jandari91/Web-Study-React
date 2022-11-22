af9bda6db86411961b674053db4220480a958f58
박영석 <yeongseok.park@mirero.co.kr>
2022-11-23 08:59:56 +0900

    4.7 강 완료

--- VanillaJS/Learn/Events/app.js
+++ VanillaJS/Learn/Events/app.js
@@ -1,2 +1,13 @@
-const title = document.querySelector(".hello");
+const title = document.querySelector("div.hello:first-child h1");
 
+console.log(title);
+title.innerHTML = "Hello";
+
+
+
+
+function handleTitleClick(){
+    title.style.color = "blue"
+}
+
+title.addEventListener("click", handleTitleClick);
